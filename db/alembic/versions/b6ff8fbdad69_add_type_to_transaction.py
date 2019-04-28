680a90e349a2a174d0a7a092abb9dc6050f418c1
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-04-28 18:30:40 +0200

    initial commit

---
+++ db/alembic/versions/b6ff8fbdad69_add_type_to_transaction.py
@@ -0,0 +1,24 @@
+"""add type to transaction
+
+Revision ID: b6ff8fbdad69
+Revises: 3ea1c64bdc82
+Create Date: 2019-04-28 17:52:21.066050
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b6ff8fbdad69'
+down_revision = '3ea1c64bdc82'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

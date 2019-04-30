04daa8b3c6bf0f686423b9724093cd39dc65f832
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-04-30 20:25:53 +0200

    added alembic + flask-alchemy

--- pft.py
+++ pft.py
@@ -1,10 +1,8 @@
-
-from db.pftdb import PFTdb
+from app.dbutils import add_new_transaction
 
 class PersonalFinanceTool():
     def __init__(self, db_name="db/pft.sqlite"):
-        self.db = PFTdb(db_name)
-        self.db.add_new_transaction(price=10.99, comment="more beer")
+        add_new_transaction(price=10.99, comment="more beer")
 
 if __name__ == '__main__':
     ptf = PersonalFinanceTool()

a41b9e3ee7409bf5894260f346b93193f8a602ba
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-04-30 21:00:35 +0200

    added basic datetime conversion

--- pft.py
+++ pft.py
@@ -1,8 +1,10 @@
 from app.dbutils import add_new_transaction
+from tools.dateutils import convert_to_datetime
 
 class PersonalFinanceTool():
     def __init__(self, db_name="db/pft.sqlite"):
-        add_new_transaction(price=10.99, comment="more beer")
+        dt = convert_to_datetime("30", "06", "1994")
+        add_new_transaction(price=10.99, comment="more beer", date=dt)
 
 if __name__ == '__main__':
     ptf = PersonalFinanceTool()

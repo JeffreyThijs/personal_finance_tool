1da260bcd0e0044f934d456bbd90e0f60ac89ac4
Jeffrey Thijs <jeffrey.thijs@hotmail.com>
2019-05-09 21:15:35 +0200

    added base net wage calculator

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request, send_file
 from flask_login import login_user, logout_user, current_user, login_required
-from app.forms import LoginForm, RegistrationForm, TransactionForm, TranactionButton
+from app.forms import LoginForm, RegistrationForm, TransactionForm, TranactionButton, TaxForm
 from app.tools.dateutils import filter_on_MonthYear, _next_month, _previous_month, dt_parse
+from app.tools.taxutils import calc_net_wage
 from app.tools.plotutils import test_plot, _plot
 from app.dbutils import add_new_transaction
 from werkzeug.urls import url_parse
@@ -135,6 +136,14 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/tax', methods=['GET', 'POST'])
+@login_required
+def tax():
+    form = TaxForm()
+    net_wage = -1
+    if form.validate_on_submit():
+        net_wage = calc_net_wage(form.gross_wage.data)
+    return render_template('tax.html', title='Tax', form=form, net_wage=net_wage)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():

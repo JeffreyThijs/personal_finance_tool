64c63f002da2afcc2c9d1d33061ad0e67724f02c
Jeffrey Thijs <jeffrey.thijs@hotmail.com>
2019-07-28 20:49:01 +0200

    added docker config

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ home_dir = str(Path.home())
 class Config(object):
     SECRET_KEY = "o8aBy0lrI7EsC8kpIExQMkznaHXdStMP38y10BtEcJ4Tlz6WlQhApxPafHcuelY6fQN6oUJdE6NNGWBoPzfZxX3hTuuMBZvPrn1vKbZB1OZT9AWu9XqapPRN0xu3cMUV"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(home_dir, '.pftdb', 'pft.sqlite')
+        'sqlite:///' + os.getcwd() + '/' + os.path.join('app', 'db', 'pft.sqlite')
+        # 'sqlite:///' + os.path.join(home_dir, '.pftdb', 'pft.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

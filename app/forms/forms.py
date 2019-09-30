c43229a0d59e4cdd4d2deb0563b4d225ca9314ff
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-09-30 21:38:13 +0200

    updated navbar

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FloatField, SelectField, HiddenField, IntegerField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.sqldb.models import User, Transaction
-
 class ChangeDateForm(FlaskForm):
     change_date_id = HiddenField()
 

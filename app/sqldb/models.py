a84cc61c9a6701a27e4c311f6cf2ab5d039a5513
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-11-12 21:15:10 +0100

    addded email support + reset password feature

--- app/sqldb/models.py
+++ app/sqldb/models.py
@@ -1,8 +1,10 @@
-from app import db, login
+from app import db, login, app
 import datetime
 import enum
+from time import time
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
 
 class FormEnum(enum.Enum):
     @classmethod
@@ -29,6 +31,21 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from pathlib import Path
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 class Config(object):
-    SECRET_KEY = "o8aBy0lrI7EsC8kpIExQMkznaHXdStMP38y10BtEcJ4Tlz6WlQhApxPafHcuelY6fQN6oUJdE6NNGWBoPzfZxX3hTuuMBZvPrn1vKbZB1OZT9AWu9XqapPRN0xu3cMUV"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')

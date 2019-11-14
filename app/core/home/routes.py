79f4ccae48c783e59dff0872068fcf8d2368f982
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-11-14 23:50:54 +0100

    base page not showing fix

--- app/core/home/routes.py
+++ app/core/home/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 from flask_login import current_user, login_required
 from app.core.home import bp
 from app.core.home.charts import get_bar_charts_data, get_donut_charts_data, get_line_charts_data
@@ -15,7 +15,7 @@ def index():
     bar_data = get_bar_charts_data(transactions)
     line_data = get_line_charts_data(transactions)
 
-    return render_template('index.html', 
+    return render_template('/core/home/index.html', 
                             line_data=line_data,
                             bar_data=bar_data,
                             donut_data=donut_data)

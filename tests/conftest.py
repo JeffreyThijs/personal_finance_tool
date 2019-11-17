a2890b2d395f7db7cf4e2bca450a607335cff373
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-11-17 17:19:43 +0100

    add functional tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,27 @@
 import pytest
 import datetime
 from app.sqldb.models import User, Transaction, Prognosis
+from config import TestConfig
+from app import create_app, db
+from faker import Faker
+from app.tools.helpers_classes import AttrDict
 
 @pytest.fixture(scope="module")
 def test_users():
     def make_test_users(amount=1):
-        users = []
-        for i in range(amount):
+
+        users = [User(id=0,
+                      username="test_user", 
+                      password="test_password", 
+                      email="test@test.test")]
+
+        for i in range(1, amount):
+            f = Faker()
+            profile = AttrDict(f.profile())
             users.append(User(id=i,
-                              username="test_user", 
-                              password="test_password", 
-                              email="test@test.test"))
+                              username=profile.username, 
+                              password=profile.job, 
+                              email=profile.mail))
         return users
     return make_test_users
 
@@ -28,3 +39,40 @@ def test_transactions():
                                             comment="placeholder"))
         return transactions
     return make_test_transactions
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app(TestConfig)
+
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    testing_client = flask_app.test_client()
+
+    # Establish an application context before running the tests.
+    ctx = flask_app.app_context()
+    ctx.push()
+
+    yield testing_client  # this is where the testing happens!
+
+    ctx.pop()
+
+
+@pytest.fixture(scope='module')
+def init_database(test_users):
+
+    # create users
+    new_users = test_users(amount=10)
+
+    # Create the database and the database table
+    db.create_all()
+
+    # Insert user data
+    for user in new_users:
+        db.session.add(user)
+
+    # Commit the changes for the users
+    db.session.commit()
+
+    yield db  # this is where the testing happens!
+
+    db.drop_all()

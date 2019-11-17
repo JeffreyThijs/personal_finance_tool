7ed0d4d0c52d1eac008d71f59f1eadad6e1a2e06
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-11-17 17:19:20 +0100

    adedd extra message flashes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,4 @@
+from flask import flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
@@ -19,11 +20,13 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
+            flash('Username already used!', "error")
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
+            flash('Email already used!', "error")
             raise ValidationError('Please use a different email address.')
 
 

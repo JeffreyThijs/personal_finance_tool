a103bef0981be90435c0eaa6ac1eee5c1e89ba56
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-11-18 21:56:06 +0100

    added tags to prognosis

--- app/core/prognosis/routes.py
+++ app/core/prognosis/routes.py
@@ -3,18 +3,28 @@ from flask_login import login_required
 from app.core.prognosis import bp
 from app.core.prognosis.form_handler import FormHandler
 from app.core.prognosis.helpers import get_prognosis_data
+import datetime
 
 @bp.route('/yearly_overview', methods=['GET', 'POST'])
 @login_required
 def yearly_overview():
     # init form handler
     f = FormHandler()
+
+    # get year
+    year = datetime.datetime.now().year
+    
     # get prognoses
-    data = get_prognosis_data(2019)
+    data = get_prognosis_data(year)
+    
+    # make table title
+    table_title = "Prognosis " + str(year)
+
     # handle forms
     if f.handle_forms():
         return redirect(url_for('prognosis.yearly_overview'))  
 
     return render_template('core/prognosis/yearly_overview.html',
+                           table_title=table_title,
                            data=data,
                            forms=f.forms)

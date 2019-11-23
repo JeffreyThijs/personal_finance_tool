c5ee7dedf9ba379e6477e977a42eebdb139e3b31
JeffreyThijs <jeffrey.thijs@hotmail.com>
2019-11-23 18:04:09 +0100

    fixed onclick module function issue

--- app/static/js/alter_forms.js
+++ app/static/js/alter_forms.js
@@ -1,4 +1,26 @@
-import { parse_object } from './object_parsing.js';
+function parse_object(x) {
+    function to_object(kv_pairs) {
+        var obj = {};
+        for (var i = 0; i < kv_pairs.length; i++) {
+            obj[kv_pairs[i][0]] = kv_pairs[i][1];
+        }
+        return obj;
+    }
+
+    function get_key_value(value, index, array) {
+        var kv = value.split(":")
+        return [kv[0].trim(), kv.slice(1).join(":").trim()];
+    }
+
+    x = x.slice(1, x.length - 1);
+    var objects = x.split(",");
+    var kv_pairs = objects.map(get_key_value);
+    return to_object(kv_pairs);
+}
+
+function get_type(obj) {
+    return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+}
 
 function obtain_prognosis_info(prognosis) {
     console.log(prognosis);

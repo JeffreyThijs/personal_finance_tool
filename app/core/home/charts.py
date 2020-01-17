f120169c8ec99588d68149842501ba574d5cf193
JeffreyThijs <jeffrey.thijs@hotmail.com>
2020-01-17 22:49:20 +0100

    changed bar data to show last 12 month data instead of months of current year

--- app/core/home/charts.py
+++ app/core/home/charts.py
@@ -1,4 +1,4 @@
-from app.tools.dateutils import filter_on_MonthYear, MONTHS, partition_in_MonthYear
+from app.tools.dateutils import filter_on_MonthYear, MONTHS, partition_in_MonthYear, filter_on_last_x_months, get_x_prev_months
 from app.sqldb.models import Transaction
 from app import cache
 import datetime
@@ -25,28 +25,46 @@ def get_donut_charts_data(transactions):
     return donut_data
 
 @cache.memoize(timeout=300)
-def get_bar_charts_data(transactions, year=datetime.datetime.now().year):
+def get_bar_charts_data(transactions, last_x_months=12):
 
-    bar_data = {"labels" : MONTHS,
-                "incoming" : [],
-                "outgoing" : []}
+    bar_data = {"labels" : [0.0] * last_x_months,
+                "incoming" : [0.0] * last_x_months,
+                "outgoing" : [0.0] * last_x_months,
+                "expected_incoming" :  [0.0] * last_x_months,
+                "expected_outgoing" :  [0.0] * last_x_months}
 
-    for month in bar_data["labels"]:
-        sum_incoming, sum_outgoing = 0, 0
-        ts = filter_on_MonthYear(transactions, "date", month, str(year))
-        for t in ts:
-            if t.incoming:
-                sum_incoming += t.price
-            else:
-                sum_outgoing += t.price
+    ts = filter_on_last_x_months(transactions, last_x_months)
+    ts = partition_in_MonthYear(ts)
+    last_ordered_x_months, last_ordered_x_months_year = get_x_prev_months(last_x_months)
 
-        bar_data["incoming"].append(round(sum_incoming, 2))
-        bar_data["outgoing"].append(round(sum_outgoing, 2))
+    unique_years = list(set(last_ordered_x_months_year))
+    prognosis_data = {}
+    for year in unique_years:
+        prognosis_data[year] = get_prognosis_data(int(year))
 
-    # get prognoses
-    prognosis_data = get_prognosis_data(datetime.datetime.now().year)
-    bar_data["expected_incoming"] = [prognosis_data[x].incoming for x in prognosis_data.keys() if x != "Totals"]
-    bar_data["expected_outgoing"] = [prognosis_data[x].outgoing for x in prognosis_data.keys() if x != "Totals"]
+    for i, (month, year) in enumerate(zip(last_ordered_x_months, last_ordered_x_months_year)):
+        
+        sum_incoming, sum_outgoing = 0.0, 0.0
+
+        if (year in ts) and (month in ts[year]):
+            for t in ts[year][month]:
+                if t.incoming:
+                    sum_incoming += t.price
+                else:
+                    sum_outgoing += t.price
+
+        bar_data["labels"][i] = month + " " + year
+        bar_data["incoming"][i] = round(sum_incoming, 2)
+        bar_data["outgoing"][i] = round(sum_outgoing, 2)
+
+        sum_incoming, sum_outgoing = 0.0, 0.0
+
+        if (year in prognosis_data) and (month.capitalize() in prognosis_data[year]):
+            sum_incoming += prognosis_data[year][month.capitalize()]["incoming"]
+            sum_outgoing += prognosis_data[year][month.capitalize()]["outgoing"]
+
+        bar_data["expected_incoming"][i] = round(sum_incoming, 2)
+        bar_data["expected_outgoing"][i] = round(sum_outgoing, 2)
 
     return bar_data
 
@@ -59,7 +77,7 @@ def get_line_charts_data(transactions):
     line_data["current_balance"] = (sum([t.price for t in transactions if (t.incoming and (t.date < datetime.datetime.now()))]) -
                                     sum([t.price for t in transactions if (not t.incoming and (t.date < datetime.datetime.now()))]))
     line_data["current_balance"] = round(line_data["current_balance"], 2)
-    transactions_monthly = partition_in_MonthYear(transactions, "date")
+    transactions_monthly = partition_in_MonthYear(transactions)
 
     _balance = 0
     for year in transactions_monthly:
--- app/tools/dateutils.py
+++ app/tools/dateutils.py
@@ -1,5 +1,7 @@
 import datetime
 from dateutil import relativedelta
+import numpy as np
+from collections import deque
 
 MONTHS = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
           'august',  'september', 'october', 'november', 'december']
@@ -77,7 +79,6 @@ def date_parse(date: str, seperator="-", output_type="tuple", reverse_date = Fal
         raise ValueError("unknown output type: {}".format(output_type))
 
 def filter_on_datetime(objects,
-                       datetime_attr: str,
                        begin_date: str,
                        end_date: str):
 
@@ -89,28 +90,76 @@ def filter_on_datetime(objects,
     end_dt = convert_to_datetime(end_day, end_month, end_year,
                                  "23", "59", "59")
 
-    return list(filter(lambda o: (getattr(o, datetime_attr) <= end_dt) and
-                       (getattr(o, datetime_attr) >= start_dt), objects))
+    return list(filter(lambda o: (o.date <= end_dt) and (o.date >= start_dt), objects))
 
+def partition_in_MonthYear(datetime_objects):
 
-def partition_in_MonthYear(objects, 
-                           datetime_attr : str):
+    dt_dict = dict()
 
-    object_dict = dict()
-
-    for o in objects:
-        dt = getattr(o, datetime_attr)
+    for datetime_object in datetime_objects:
+        dt = datetime_object.date
         year_str = str(dt.year)
         month_str = MONTHS[dt.month - 1]
 
-        if year_str not in object_dict:
-            object_dict[year_str] = dict()
+        if year_str not in dt_dict:
+            dt_dict[year_str] = dict()
         
-        if month_str not in object_dict[year_str]:
-            object_dict[year_str][month_str] = []
+        if month_str not in dt_dict[year_str]:
+            dt_dict[year_str][month_str] = []
 
-        object_dict[year_str][month_str].append(o)
-    return object_dict
+        dt_dict[year_str][month_str].append(datetime_object)
+
+    return dt_dict
+
+
+def filter_on_last_x_months(objects, x_months):
+
+    if not isinstance(x_months, int) or x_months < 1:
+        raise ValueError("invalid months")
+
+    start_month = change_month(datetime.datetime.now().month, -x_months + 1)
+    year_compensation = 1 if datetime.datetime.now().month < start_month else 0
+    start_year = datetime.datetime.now().year - (np.floor((x_months - 1) / 12).astype(int)) - year_compensation
+    end_month = datetime.datetime.now().month
+    end_year = datetime.datetime.now().year
+    
+    end_day = str(get_days_in_month(int(end_month), int(end_year)))
+
+    begin_date = "01" + "-" + str(start_month) + "-" + str(start_year)
+    end_date = end_day + "-" + str(end_month) + "-" + str(end_year)
+
+    print(begin_date)
+    print(end_date)
+
+    return filter_on_datetime(objects=objects,
+                              begin_date=begin_date,
+                              end_date=end_date)
+
+def get_x_prev_months(x : int):
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("invalid input")
+
+    current_month = datetime.datetime.now().month
+    prev_months = MONTHS * int(1 + (x - 1) / 12)
+    prev_years = [0] * x
+
+    # put months in right order
+    prev_months = deque(prev_months)
+    prev_months.rotate(-current_month)
+    prev_months = list(prev_months)
+
+    _year = datetime.datetime.now().year
+    for i, month in enumerate(reversed(prev_months)):
+        print(month)
+        if month == MONTHS[-1]:
+            _year -= 1
+        prev_years[i] = str(_year)
+    prev_years.reverse()
+
+    # only keep last x values
+    return prev_months[-x:], prev_years[-x:]
+
 
 def filter_on_MonthYear(objects,
                         datetime_attr : str,
@@ -123,7 +172,6 @@ def filter_on_MonthYear(objects,
     end_date = end_day+"-"+month+"-"+year
 
     return filter_on_datetime(objects=objects,
-                              datetime_attr=datetime_attr,
                               begin_date=begin_date,
                               end_date=end_date)
 
@@ -144,8 +192,8 @@ def convert_month(month: str) -> str:
     else:
         raise ValueError("Invalid input")
 
-def change_month(month : str, offset : int = 0) -> int:
-    if month.lower() in MONTHS: month = convert_month(month)
+def change_month(month, offset : int = 0) -> int:
+    if isinstance(month, str) and (month.lower() in MONTHS): month = convert_month(month)
     return ((((int(month)-1) + offset) % 12) + 1)
 
 def _previous_month(month : str) -> int:

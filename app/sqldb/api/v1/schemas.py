f0db63884f872ca8f00451a5840e32797a14dce4
JeffreyThijs <jeffrey.thijs@hotmail.com>
2020-01-27 17:33:00 +0100

    refactored some api stuff in preload func in schemas

--- app/sqldb/api/v1/schemas.py
+++ app/sqldb/api/v1/schemas.py
@@ -12,6 +12,18 @@ class UserRegistrationSchema(Schema):
     email = fields.Email()
     password = fields.Str()
 
+    @pre_load
+    def check_user(self, data, **kwargs):
+        # check username
+        user = User.query.filter_by(username=data['username']).first()
+        if user is not None:
+            raise ValueError('Please use a different username.')
+
+        # check email
+        user = User.query.filter_by(email=data['email']).first()
+        if user is not None:
+            raise ValueError('Please use a different email address.')
+
     @post_load
     def make_user(self, data, **kwargs):
         return User(**data)
@@ -19,6 +31,17 @@ class UserRegistrationSchema(Schema):
 class UserLoginSchema(Schema):
     username = fields.Str()
     password = fields.Str()
+
+    @pre_load
+    def check_user(self, data, **kwargs):
+        # check username
+        user = User.query.filter_by(username=data['username']).first()
+        if user is None:
+            raise ValueError('User does not exist')
+
+        # check password
+        if not user.check_password(data['password']):
+            raise ValueError('Wrong password')
 
     @post_load
     def make_user(self, data, **kwargs):

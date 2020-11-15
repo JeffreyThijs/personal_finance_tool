b899dd9d487e549f29a8115740091048e526db8b
JeffreyThijs <jeffrey.thijs@hotmail.com>
2020-11-15 14:46:41 +0100

    use sqlalchemy_to_pydantic for transaction schemes

--- app/storage/schemas/transactions.py
+++ app/storage/schemas/transactions.py
@@ -1,35 +1,25 @@
-from datetime import datetime
-from pydantic.main import BaseModel
-from pydantic import UUID4 as GUID
 from pydantic_sqlalchemy import sqlalchemy_to_pydantic
-# from ..models import TransactionTable as Transaction
+from ..models import TransactionTable as Transaction
 
-class BaseTransaction(BaseModel):
-    date: datetime
-    comment: str
-    price: float
-    
-class BaseTransactionWithId(BaseTransaction):
-    id: int
-    
-class TransactionDB(BaseTransactionWithId):
-    created_at: datetime
-    updated_at: datetime
-    user_id: GUID
-    
-class TransactionCreate(BaseTransaction):
+_TransactionDB = sqlalchemy_to_pydantic(Transaction)
+_TransactionCreate = sqlalchemy_to_pydantic(
+    Transaction, exclude=["id", "user_id", "updated_at", "created_at"])
+_TransactionUpdate = sqlalchemy_to_pydantic(
+    Transaction, exclude=["id", "user_id", "updated_at", "created_at"])
+_TransactionOut = sqlalchemy_to_pydantic(
+    Transaction, exclude=["user_id", "updated_at", "created_at"])
+
+class TransactionDB(_TransactionDB):
     pass
 
-class TransactionUpdate(BaseTransaction):
+
+class TransactionCreate(_TransactionCreate):
     pass
 
-class TransactionOut(BaseTransactionWithId):
-    
-    class Config:
-        orm_mode = True
-    
 
-# TransactionDB = sqlalchemy_to_pydantic(Transaction)
-# TransactionCreate = sqlalchemy_to_pydantic(Transaction, exclude=["id", "user_id", "updated_at", "created_at"])
-# TransactionUpdate = sqlalchemy_to_pydantic(Transaction, exclude=["id", "user_id", "updated_at", "created_at"])
-# TransactionOut = sqlalchemy_to_pydantic(Transaction, exclude=["id", "user_id", "updated_at", "created_at"])
+class TransactionUpdate(_TransactionUpdate):
+    pass
+
+
+class TransactionOut(_TransactionOut):
+    pass

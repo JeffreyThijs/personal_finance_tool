5ebce4536e21bf49dbc07e4551d7c31ed078b4bb
JeffreyThijs <jeffrey.thijs@hotmail.com>
2020-11-15 13:36:48 +0100

    add min password length constraint

--- app/storage/schemas/users.py
+++ app/storage/schemas/users.py
@@ -1,15 +1,18 @@
+from typing import Optional
 from fastapi_users import models
+from pydantic.types import constr
+
 
 class User(models.BaseUser, models.BaseOAuthAccountMixin):
     pass
 
 
 class UserCreate(models.BaseUserCreate):
-    pass
+    password: constr(min_length=8)
 
 
 class UserUpdate(User, models.BaseUserUpdate):
-    pass
+    password: Optional[constr(min_length=8)]
 
 
 class UserDB(User, models.BaseUserDB):

0a72237a920f7d03d7ea1658c835c0dcb940d7c4
JeffreyThijs <jeffrey.thijs@hotmail.com>
2020-11-20 22:13:16 +0100

    simple export

--- app/core/home/routes.py
+++ app/core/home/routes.py
@@ -11,8 +11,14 @@ def index():
     donut_data = get_donut_charts_data()
     bar_data = get_bar_charts_data()
     line_data = get_line_charts_data()
+    
+    from app import db
+    from app.sqldb.models import Transaction
+    
+    all_data = db.session.query(Transaction).filter(Transaction.user_id == current_user.id).all()
 
-    return render_template('/core/home/index.html', 
+    return render_template('/core/home/index.html',
+                            all_data=all_data,
                             line_data=line_data,
                             bar_data=bar_data,
                             donut_data=donut_data)

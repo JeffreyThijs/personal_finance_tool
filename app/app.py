b4305c5dbc62296239d2ff77f500dcdcc43f3825
JeffreyThijs <jeffrey.thijs@hotmail.com>
2020-11-21 13:43:16 +0100

    add cors origin frontend

--- app/app.py
+++ app/app.py
@@ -17,7 +17,8 @@ origins = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "http://127.0.0.1",
-    "http://localhost:8080"
+    "http://localhost:8080",
+    "https://pft-webapp.herokuapp.com",
 ]
 
 app.add_middleware(

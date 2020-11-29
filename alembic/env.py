0cfa1499fee2e2ed82310dfd814aa2f196736c67
JeffreyThijs <jeffrey.thijs@hotmail.com>
2020-11-29 12:12:18 +0100

    get database url from app settings for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,11 +5,11 @@ from sqlalchemy import pool
 
 from alembic import context
 import os, sys
-from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
 sys.path.append(BASE_DIR)
+
+from app.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +17,7 @@ config = context.config
 
 # this will overwrite the ini-file sqlalchemy.url path
 # with the path given in the config of the main code
-config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_utils.timing import add_timing_middleware
 
 from .api.api_v1.api import api_router
 from .api.common.api import api_router as common_api_router
 from .storage.user_db import database
+from .settings import settings
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI(docs_url="/")
 
@@ -25,6 +31,7 @@ CORSMiddleware,
     allow_methods=["*"], # Allows all methods
     allow_headers=["*"], # Allows all headers
 )
+add_timing_middleware(app, record=logger.info, prefix="app")
 
 app.include_router(common_api_router)
 app.include_router(api_router)
